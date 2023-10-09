75f719f66ff0aff97feb9747e2af06e9611fb195
mw491 <musaiw@outlook.com>
2023-10-09 19:44:51 +0300

    change text for chelseafc

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
       'clamp-big': "clamp(2rem, 10vw, 10rem)",
       'clamp-mid': "clamp(1.5rem, 8vw, 6rem)",
       'clamp-small': "clamp(1rem, 5vw, 3rem)",
+      'clamp-xs': "clamp(0.25rem, 3vw, 2rem)",
     },
   },
   plugins: [],
