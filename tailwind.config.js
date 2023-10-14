0f18f57234ff918ceabf768d1c6007df5a3c4082
mw491 <musaiw@outlook.com>
2023-10-14 12:23:15 +0300

    change font to lato

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./*.html",
-  ],
+  content: ["./*.html"],
   theme: {
     fontFamily: {
-      'brand': ['Open Sans'],
+      brand: ["Lato"],
     },
     fontSize: {
-      'clamp-big': "clamp(2rem, 10vw, 10rem)",
-      'clamp-mid': "clamp(1.5rem, 8vw, 6rem)",
-      'clamp-small': "clamp(1rem, 5vw, 3rem)",
-      'clamp-xs': "clamp(0.25rem, 3vw, 2rem)",
+      "clamp-big": "clamp(2rem, 10vw, 10rem)",
+      "clamp-mid": "clamp(1.5rem, 8vw, 6rem)",
+      "clamp-small": "clamp(1rem, 5vw, 3rem)",
+      "clamp-xs": "clamp(0.25rem, 3vw, 2rem)",
     },
   },
   plugins: [],
-}
+};
